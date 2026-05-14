879938a45bf96fcd8d62c7e926e8078bbcb99a27
시원 <96858320+NearthYou@users.noreply.github.com>
2026-05-14 11:41:28 +0900

    fix: rollback_frame free frame->kva

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -248,7 +248,7 @@ static void rollback_frame (struct page *page, struct frame *frame) {
 	list_remove(&frame->elem);
 	lock_release(&frame_table_lock);
 
-	palloc_free_page(frame->kva);
+	//palloc_free_page(frame->kva);
 
 	free(frame);
 }
