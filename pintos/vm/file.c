8f0dea1eb24424d3fd81eea9e99e3a20e2773430
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-05-19 22:50:43 +0900

    feat: munmap 슈도코드 작성

--- pintos/vm/file.c
+++ pintos/vm/file.c
@@ -1,6 +1,8 @@
 /* file.c: 메모리 기반 파일 객체(mmap된 객체)의 구현. */
 
 #include "vm/vm.h"
+#include "threads/mmu.h"
+#include "userprog/process.h"
 
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
@@ -31,7 +33,8 @@ file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
 /* 파일에서 내용을 읽어 페이지를 스왑 인합니다. */
 static bool
 file_backed_swap_in (struct page *page, void *kva) {
-	struct file_page *file_page UNUSED = &page->file;
+	struct file_page *file_page
+	UNUSED = &page->file;
 }
 
 /* 파일에 내용을 다시 써서 페이지를 스왑 아웃합니다. */
@@ -55,4 +58,25 @@ do_mmap (void *addr, size_t length, int writable,
 /* munmap을 수행합니다. */
 void
 do_munmap (void *addr) {
+	// munmap은 페이지를 해제하는 함수임. munmap이 호출되면, 해당 페이지가 SPT에서 제거되고, 페이지가 점유한 프레임이 해제되어야 함.
+	// 1. 일단 addr 기반으로 SPT에서 페이지를 찾음.
+	spt_find_page(thread_current()->spt, addr);
+	// 2. VM_FILE인지 check
+	if (FILE != VM_FILE)
+		return false;
+	// 3. memory에 올라와 있는지 check
+	if (pa != kva)
+		return false;
+	// 4. pml4 매핑 제거함
+	pml4_clear_page();
+	// 5. SPT에서 제거함.
+	spt_remove_page();
+	// 6. page, frame, aux free 해줌.
+	free(page);
+	free(frame);
+	free(aux);
+	// 7. mmap용 파일 close
+	close(file);
+	// 8. process_exit ()
+	process_exit();
 }
