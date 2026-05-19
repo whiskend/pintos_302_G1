d67cb39e5f6185a4334d41cb54d5f231740516e2
이경근 <lsg008@naver.com>
2026-05-20 00:19:43 +0900

    fix: spt에 이미 있는 경우 실패

--- pintos/userprog/syscall.c
+++ pintos/userprog/syscall.c
@@ -481,6 +481,8 @@ syscall_handler (struct intr_frame *f) {
 				goto fail;
 			if (fd == STDIN_FILENO || fd == STDOUT_FILENO) 
 				goto fail;
+			if (!spt_find_page(thread_current()->spt, addr))
+				goto fail;
 
 			addr = pg_round_down (addr);
 
